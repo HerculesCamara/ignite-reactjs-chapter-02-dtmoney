cce7554b1b9cb3de070e41221a91afdac3b18994
Hércules Câmara <39931772+HerculesCamara@users.noreply.github.com>
2021-10-14 22:31:29 -0300

    Transaction listing

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import { api } from "../../services/api";
+
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 interface NewTransactionModalProps {
